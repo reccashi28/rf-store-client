604967c8cb358e249e0d8d545cc63b89ca52f8e3
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-07-26 18:52:26 +0200

    style footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div style={{backgroundColor: '#00334e', color: 'white', bottom: 0, width: '100%', padding: '10px', marginTop: '20px'}}>
-           <Box display='flex' justifyContent='center' alignItems='center'>
+        <div style={{backgroundColor: '#00334e', color: 'white', bottom: 0, width: '100%', padding: '10px', marginTop: '20px', borderTopLeftRadius: '5px', borderTopRightRadius: '5px'}}>
+           <Box display='flex' justifyContent='center' alignItems='center' style={{height: '100px'}}>
                 <Typography>All rights reserved © 2021</Typography>
             </Box>
         </div>
